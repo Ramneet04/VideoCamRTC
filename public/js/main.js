98f0efdc6651808cfd37379af57698ab02a5f5b2
Ramneet04 <ramneetsinghrtd@gmail.com>
2025-01-28 01:38:37 +0530

    video-cam finish

--- public/js/main.js
+++ public/js/main.js
@@ -7,6 +7,7 @@ const endCallBtn = document.getElementById("end-call-btn");
 const socket = io();
 let curruser="";
 let localStream;
+let caller=[];
 const peerConnection = (function(){
     let peerConnection;
     const createPeerConnection = ()=>{
@@ -20,10 +21,20 @@ const peerConnection = (function(){
         peerConnection = new RTCPeerConnection(config);
 
         //add localstream to peerConection
-        localStream.getTracks().array.forEach(track => {
+        localStream.getTracks().forEach(track => {
             peerConnection.addTrack(track,localStream);
         });
-        //listento remote stream and add to peer connection
+        //listen to remote stream and add to peer connection
+        peerConnection.ontrack = (event) =>{
+            remoteVideo.srcObject = event.streams[0];
+        }
+        //listen for ice candidate
+        peerConnection.onicecandidate = (event) =>{
+            if(event.candidate){
+                socket.emit("icecandidate", event.candidate);
+            }
+        }
+        return peerConnection;
     }
     return {
         getInstances: ()=>{
@@ -44,6 +55,9 @@ createUserBtn.addEventListener("click", (e)=>{
     }
 
     username.value = "";
+})
+endCallBtn.addEventListener("click",(e)=>{
+    socket.emit("call-ended", caller)
 })
 
 socket.on("joined",(allusers)=>{
@@ -77,11 +91,59 @@ socket.on("joined",(allusers)=>{
     createUsersHtml();
 });
 
+socket.on("offer", async ({from,to,offer})=>{
+    const pc = peerConnection.getInstances();
+    // set remote description
+    await pc.setRemoteDescription(offer);
+    // now remote machine will create an answer
+    const answer = await pc.createAnswer();
+    await pc.setLocalDescription(answer);
+    console.log("Offer received:", offer);
+    console.log("Answer received:", answer);
+    socket.emit("answer", {from, to, answer: pc.localDescription});
+    caller= [from,to];
+})
+
+socket.on("answer",async ({from,to,answer})=>{
+    const pc = peerConnection.getInstances();
+    // set remote description
+    await pc.setRemoteDescription(answer);
+    endCallBtn.style.display = "block";
+    socket.emit("end-call", {from,to});
+    caller= [from,to];
+})
+
+socket.on("icecandidate", async (candidate)=>{
+    console.log(candidate);
+    const pc = peerConnection.getInstances();
+    await pc.addIceCandidate(new RTCIceCandidate(candidate));
+})
+
+socket.on("end-call", ({from,to})=>{
+    // no need of {from,to} as we are broadcasting or sending the event t that person only..
+    endCallBtn.style.display = "block";
+})
+
+socket.on("call-ended", (caller)=>{
+    endCall();
+})
 //start call method
-const startCall = (user)=>{
+const startCall = async (user)=>{
     console.log({user});
+    const pc = peerConnection.getInstances();
+    const offer = await pc.createOffer();
+    console.log(offer);
+    await pc.setLocalDescription(offer);
+    socket.emit("offer", {from: curruser, to: user, offer: pc.localDescription});
 }
 
+const endCall = async ()=>{
+    const pc = peerConnection.getInstances();
+    if(pc){
+        pc.close();
+        endCallBtn.style.display="none";
+    }
+}
 const startMyVideo = async ()=>{
     try {
         const stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: true});
